7993b7dce70a8b0008ad8d85d490de0912b58ec7
sajadbanooie <sajadbanooie@yahoo.com>
2015-05-09 06:05:20 -0700

    Waiting

--- src/Pentago/game.java
+++ src/Pentago/game.java
@@ -5,6 +5,7 @@ public class game {
 	private board Board;
 	private player white, black;
 	public Client client;
+	private move current = null;
 	
 	public game(player one, player two)
 	{
@@ -29,6 +30,15 @@ public class game {
 	
 	int timeout=0;
 	boolean exception=false;
+	public void _wait(){
+		synchronized (this) {
+			try {
+				this.wait(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	public void start()
 	{
 		move m;
@@ -36,7 +46,14 @@ public class game {
 		int now=0;
 		while(Board.winner() == 0)
 		{
-			m = getMove(players[now], Board);
+			try {
+				_wait();
+				getMove(players[now],Board);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			m = current;
 			if (m==null)
 			{
 				timeout = 2 - now;
@@ -66,33 +83,23 @@ public class game {
 	}
 	
 	private move m;
-	private move getMove(final player p, final board board)
-	{
-		m=null;
+	private void getMove(final player p, final board board) throws InterruptedException {
 		Thread t=new Thread()
 		{
 			public void run()
 			{
 				try
 				{
-					m = p.getMove(board);
+					current = p.getMove(board);
 				}
 				catch (Exception e)
 				{
 					e.printStackTrace();
 					exception=true;
-					m=null;
+					current=null;
 				}
 			}
 		};
 		t.start();
-		try
-		{
-			t.join(1000);
-		} catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		return m;
 	}
 }
